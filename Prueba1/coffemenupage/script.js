4d1e4967a0b9ad7b51435738d17751212aabe9f4
tomasnazer <104788616+tomasnazer@users.noreply.github.com>
2022-05-07 12:04:56 -0300

    added json data

--- Prueba1/coffemenupage/script.js
+++ Prueba1/coffemenupage/script.js
@@ -1,7 +1,35 @@
+import data from "../data/cafe.json" assert {type: "json"};
+
 const btnMenu = document.querySelector("#menu-icon");
 const menu = document.querySelector("#smenu");
+
+console.log(data)
 
 btnMenu.addEventListener("click", function(e){
     e.preventDefault();
     menu.classList.toggle('show')
 });
+
+
+data.cafe.forEach(cafe => {
+    if(cafe.stock === "true"){
+    document.getElementById('cafeteria').insertAdjacentHTML('beforeend',
+    `
+    <picture>
+        <img class="img-box" src="${cafe.img}" alt="coffe-pic">
+    </picture>
+    <span class="product">${cafe.nombre} $${cafe.precio}</span>
+    <span class="description">${cafe.descripcion}</span>
+    `
+    )} else {
+        document.getElementById('cafeteria').insertAdjacentHTML('beforeend',
+        `
+        <picture>
+            <img class="img-box" src="${cafe.img}" alt="coffe-pic">
+        </picture>
+        <span class="product">${cafe.nombre} $${cafe.precio}</span>
+        <span class="description">SIN STOCK!</span>`
+        )}
+});
+
+//document.getElementById('cafeteria').appendChild('<ol><li>html data</li></ol>');
