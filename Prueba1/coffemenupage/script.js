7564a6f3c599f4e0cffa076ba4664d236d49e3eb
tomasnazer <104788616+tomasnazer@users.noreply.github.com>
2022-05-10 10:43:58 -0300

    added json data
    added more json data to remove some html text

--- Prueba1/coffemenupage/script.js
+++ Prueba1/coffemenupage/script.js
@@ -3,7 +3,6 @@ import data from "../data/cafe.json" assert {type: "json"};
 const btnMenu = document.querySelector("#menu-icon");
 const menu = document.querySelector("#smenu");
 
-console.log(data)
 
 btnMenu.addEventListener("click", function(e){
     e.preventDefault();
@@ -42,3 +41,34 @@ data.cafe.forEach(cafe => {
 });
 
 //document.getElementById('cafeteria').appendChild('<ol><li>html data</li></ol>');
+
+
+data.Capuccinos.forEach(Capuccinos => {
+    if(Capuccinos.stock === "true"){ if(Capuccinos.img != "") {
+    document.getElementById('Capuccinos').insertAdjacentHTML('beforeend',
+    `
+     
+    <picture>
+        <img class="img-box" src="${Capuccinos.img}" alt="Capuccinos-pic">
+    </picture>
+    <span class="product">${Capuccinos.nombre} $${Capuccinos.precio}</span>
+    <span class="description">${Capuccinos.descripcion}</span>
+    `
+    )} else  {document.getElementById('Capuccinos').insertAdjacentHTML('beforeend',
+        `<span class="product">${Capuccinos.nombre} $${Capuccinos.precio}</span>
+        <span class="description">${Capuccinos.descripcion}</span>`
+)}} else { if(Capuccinos.img !=""){
+        document.getElementById('Capuccinos').insertAdjacentHTML('beforeend',
+        `
+        <picture>
+            <img class="img-box" src="${Capuccinos.img}" alt="Capuccinos-pic">
+        </picture>
+        <span class="product">${Capuccinos.nombre} $${Capuccinos.precio}</span>
+        `
+        )} else{ if(Capuccinos.stock === "false"){
+            document.getElementById('Capuccinos').insertAdjacentHTML('beforeend',
+            `<span class="product">${Capuccinos.nombre} $${Capuccinos.precio}</span>
+            <span class="description">SIN STOCK!</span>`
+)}}}
+        
+});
