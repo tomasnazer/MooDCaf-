858920b5b2e78706d582feb55b64c8893d87b06e
tomasnazer <104788616+tomasnazer@users.noreply.github.com>
2022-05-19 11:15:23 -0300

    added submenu
    added submenu to the root page

---
+++ Prueba1/script.js
@@ -0,0 +1,8 @@
+const btnMenu = document.querySelector("#menu-icon");
+const menu = document.querySelector("#smenu");
+
+
+btnMenu.addEventListener("click", function(e){
+    e.preventDefault();
+    menu.classList.toggle('show')
+});
