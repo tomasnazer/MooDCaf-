14a6c2f256269855317fb3b9b34a46869767c04a
tomasnazer <104788616+tomasnazer@users.noreply.github.com>
2022-05-24 10:54:43 -0300

    about us page updates

---
+++ Prueba1/aboutus/script.js
@@ -0,0 +1,9 @@
+//Showing menu
+
+const btnMenu = document.querySelector("#menu-icon");
+const menu = document.querySelector("#smenu");
+
+btnMenu.addEventListener("click", function(e){
+    e.preventDefault();
+    menu.classList.toggle('show')
+});
